ace4263a0e537c652286bf872c00d41e2daff2e2
Asya Tealdi <asya2698@live.it>
2025-03-14 14:36:45 +0100

    Complete actors

--- SwiftConcurrencyBootcamp/ActorsBootcamp.swift
+++ SwiftConcurrencyBootcamp/ActorsBootcamp.swift
@@ -20,15 +20,33 @@ class MyDataManager {
     var data: [String] = []
     func getRandomData() -> String? {
 
-        self.data.append(UUID().uuidString)
+        self.data.append(UUID().uuidString) //Data race
         print(Thread.current)
         return data.randomElement()
     }
 }
 
+actor MyActorDataManager {
+    static let instance = MyActorDataManager()
+    private init() { }
+
+    nonisolated let myRandomText = "Random Text"
+    var data: [String] = []
+    func getRandomData() -> String? {
+
+        self.data.append(UUID().uuidString)
+        print(Thread.current)
+        return data.randomElement()
+    }
+
+    nonisolated func getSaveData() -> String {
+        return "NEW Data"
+    }
+}
+
 struct HomeView: View {
 
-    let manager = MyDataManager.instance
+    let manager = MyActorDataManager.instance
     @State private var text: String = ""
     let timer = Timer.publish(every: 0.1, on: .main, in: .common, options: nil).autoconnect()
 
@@ -40,9 +58,16 @@ struct HomeView: View {
                 .font(.headline)
         }
         .onReceive(timer) { _ in
-            DispatchQueue.global(qos: .background).async {
-                if let data = manager.getRandomData() {
-                    DispatchQueue.main.async {
+//            DispatchQueue.global(qos: .background).async {
+//                if let data = manager.getRandomData() {
+//                    DispatchQueue.main.async {
+//                        self.text = data
+//                    }
+//                }
+//            }
+            Task {
+                if let data = await manager.getRandomData() {
+                    await MainActor.run {
                         self.text = data
                     }
                 }
@@ -53,7 +78,7 @@ struct HomeView: View {
 
 struct BrowseView: View {
 
-    let manager = MyDataManager.instance
+    let manager = MyActorDataManager.instance
     @State private var text: String = ""
     let timer = Timer.publish(every: 0.01, on: .main, in: .common, options: nil).autoconnect()
 
@@ -65,13 +90,6 @@ struct BrowseView: View {
                 .font(.headline)
         }
         .onReceive(timer) { _ in
-            DispatchQueue.global(qos: .default).async {
-                if let data = manager.getRandomData() {
-                    DispatchQueue.main.async {
-                        self.text = data
-                    }
-                }
-            }
 
         }
     }
